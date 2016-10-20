5b856710cc10bdb8a684b5949cad186fe0245cbc
David Ory <dory@mtc.ca.gov>
2016-10-20 16:27:29 -0700

    refactored prior steps, progress on controls

--- application/go_go_popsyn.bat
+++ application/go_go_popsyn.bat
@@ -3,16 +3,14 @@
 :: 2. change maz/taz and maz_original/taz_original to maz_consecutive and maz_model? 
 :: 3. experiment with JPPF settings to explore run-time improvements (note num threads *appears* to be fixed in PopGenerator class)
 :: 4. any mysql optimization that can improve runtime? (lisa task?)
-:: 5. do a few source checks on the controls, perhaps scripting the construction
 :: 8. explore impact of different sets of controls (2000 PUMS to 2010 forecast and 2010 PUMS to 2000 forecast)
-:: 9. using 2000 PUMS may require new geography crosswalk (i do not think so as the 2007-2011 PUMS uses the 2000 geographies; will need to make the adjustment to income variable)
 :: 10. any benefit to combining the 2000 and 2007/11 pums into a single, large database, and then use it to build 2000 and 2010 syn pops? 
-:: 11. determine if I can re-create the previous validation results for any set of weights
-:: 12. what about a person control at the county level with a high weight?
+:: 11. determine if I can re-create the previous validation results for any set of weights (may be that they manually adjusted the controls so that totals within categories matched?)
+:: 12. introduce a person control at the county level to avoid measure category discrepancies (will this work? if it does, remove the scaling in the validation summaries)
 :: 13. introduce control on ages 16-18 (after confirming problem exists for 2000, 2005, and 2010)
-:: 14. figure out how group quarters are handled? add cross validation on variable TYPE, but note that PUMS does not give weights to group quarters. so right now when we create the seed list, we
-::     remove vacant units and we remove group quarters. so they are not considered in the expansion. what to do about this? we could put them back in and add a county-level control. 
-::     how about we run popsyn again to only expand to group quarters. so we have a separate seed list as well as separate controls? And then append the two files?
+:: 14. for group quarters, create another set of settings and run with only MAZ or TAZ or county level controls depending on what Mike R. can provide. Perhaps use MAZ and then scale up or down
+::     using a county control. bind the two datasets to run the travel model.
+:: 15. build some batch files to run with various weight differences to explore which are best  
 
 :: ms-dos batch file to run the population synthesizer
 
