e88797d369ce08a5738f4ef51cedc5a4e569dc79
Lisa Zorn <lzorn@bayareametro.gov>
2018-03-21 12:40:17 -0700

    For building, src is on M so copy it but it doesn't go on github

--- go_go_build.bat
+++ go_go_build.bat
@@ -1,11 +1,13 @@
 
 :: path details
 set JAVA_PATH=C:\Program Files\Java\jdk1.7.0_71\bin
-set ANT_PATH=C:\Users\dory\Documents\apache-ant-1.9.7\bin
+set ANT_PATH=C:\apache-ant-1.9.7\bin
 set REPOSITORY_DIR=M:\Development\Travel Model Two\Source
 
 set PATH=%JAVA_PATH%;%ANT_PATH%
 
+:: robust copy over the source; it's not here since we cannot post it on github
+C:\windows\System32\robocopy /MIR "%REPOSITORY_DIR%\popsyn3\src" src
 call ant all
 
 
